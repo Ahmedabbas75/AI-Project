f2c89a75d6e6abed20d2f0996ae0ab3244f830c1
Ahmedabbas75 <ahmedmassa.757.pyf@gmail.com>
2023-05-03 02:29:03 +0300

    Create the main structure of the window game

---
+++ GameWindow.py
@@ -0,0 +1,77 @@
+# import module we need in project
+import pygame
+from pygame.locals import *
+
+# class snake style
+class Snake:
+    def __init__(self, Screen):
+        self.parent_screen = Screen
+        self.SnakeHead = pygame.image.load("resources/block.jpg").convert()
+        self.Window_height = 100
+        self.Window_width = 100
+
+    # set new position of the snake head base on left direction
+    def Move_left(self):
+        self.Window_height -= 10
+        self.Draw()
+
+    # set new position of the snake head base on right direction
+    def Move_right(self):
+        self.Window_height += 10
+        self.Draw()
+
+    # set new position of the snake head base on up direction
+    def Move_up(self):
+        self.Window_width -= 10
+        self.Draw()
+
+    # set new position of the snake head base on down direction
+    def Move_down(self):
+        self.Window_width += 10
+        self.Draw()
+
+    # update head of snake
+    def Draw(self):
+        self.parent_screen.fill((110, 110, 5))
+        self.parent_screen.blit(self.SnakeHead,(self.Window_height,self.Window_width))
+        pygame.display.flip() # allow changes to occur
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.Window = pygame.display.set_mode((500, 500))
+        self.snake = Snake(self.Window)
+        self.snake.Draw()
+    
+    # running window win running variable equle to True
+    def Run(self):
+        Running = True
+        while Running:
+            # allaw window to receive input from keyboard
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        Running = False
+
+                    if event.key == K_LEFT:
+                        self.snake.Move_left()
+
+                    if event.key == K_RIGHT:
+                        self.snake.Move_right()
+
+                    if event.key == K_UP:
+                        self.snake.Move_up()
+
+                    if event.key == K_DOWN:
+                        self.snake.Move_down()
+
+                # when user want to exit window game
+                elif event.type == QUIT:
+                    Running = False
+
+if __name__ == '__main__':
+    game = Game()
+    game.Run()
+
+
