c1d0992cfb71f967319a15d27d3feea8e8e773e7
Ahmedabbas75 <ahmedmassa.757.pyf@gmail.com>
2023-05-03 23:37:35 +0300

    Create the main structure of the window game

--- GameWindow.py
+++ GameWindow.py
@@ -7,37 +7,37 @@ class Snake:
     def __init__(self, Screen):
         self.parent_screen = Screen
         self.SnakeHead = pygame.image.load("resources/block.jpg").convert()
-        self.Window_height = 100
-        self.Window_width = 100
-
+        self.Snake_pos_X = 100 # X coordinate of initial position
+        self.Snake_pos_Y = 100 # Y coordinate of initial position
+        
     # set new position of the snake head base on left direction
     def Move_left(self):
-        self.Window_height -= 10
+        self.Snake_pos_X -= 10
         self.Draw()
 
     # set new position of the snake head base on right direction
     def Move_right(self):
-        self.Window_height += 10
+        self.Snake_pos_X += 10
         self.Draw()
 
     # set new position of the snake head base on up direction
     def Move_up(self):
-        self.Window_width -= 10
+        self.Snake_pos_Y -= 10
         self.Draw()
 
     # set new position of the snake head base on down direction
     def Move_down(self):
-        self.Window_width += 10
+        self.Snake_pos_Y += 10
         self.Draw()
 
     # update head of snake
     def Draw(self):
         self.parent_screen.fill((110, 110, 5))
-        self.parent_screen.blit(self.SnakeHead,(self.Window_height,self.Window_width))
+        self.parent_screen.blit(self.SnakeHead,(self.Snake_pos_X,self.Snake_pos_Y))
         pygame.display.flip() # allow changes to occur
 
 
-class Game:
+class Game_Window:
     def __init__(self):
         pygame.init()
         self.Window = pygame.display.set_mode((500, 500))
@@ -45,8 +45,7 @@ class Game:
         self.snake.Draw()
     
     # running window win running variable equle to True
-    def Run(self):
-        Running = True
+    def Run(self, Running = True):
         while Running:
             # allaw window to receive input from keyboard
             for event in pygame.event.get():
@@ -71,7 +70,7 @@ class Game:
                     Running = False
 
 if __name__ == '__main__':
-    game = Game()
-    game.Run()
+    Game = Game_Window()
+    Game.Run()
 
 
