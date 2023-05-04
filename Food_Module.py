69381ef3d6beeba981599d29136b3257d2d797d1
Ahmedabbas75 <ahmedmassa.757.pyf@gmail.com>
2023-05-05 01:18:33 +0300

    Add food to game window

--- Food_Module.py
+++ Food_Module.py
@@ -6,7 +6,7 @@ class Food:
     def __init__(self,Screen) -> None:
         self.Screen = Screen
         self.Food_pos = [120,120]
-        self.Food_image = pygame.image.load("resources/apple.jpg").convert()
+        self.Food_image =pygame.transform.smoothscale(pygame.image.load("resources/apple.jpg").convert_alpha(),(40,40)) 
 
     def Draw_Food(self):
         self.Screen.blit(self.Food_image,(self.Food_pos[0],self.Food_pos[-1]))
@@ -14,6 +14,7 @@ class Food:
 
     # move position of food randomly
     def Move_food(self):
-        print(self.Food_pos)
+        self.Food_pos = [random.randint(1,15) * 40, random.randint(1,15) * 40]
+
 
 
