b2def9d35143ccec7a0afa4dd6dd01813ebcd5f8
Ahmedabbas75 <ahmedmassa.757.pyf@gmail.com>
2023-05-05 02:47:18 +0300

    fix screen updating bug

--- GameWindow.py
+++ GameWindow.py
@@ -1,37 +1,43 @@
 # import module we need in project
-from pygame.locals import *
+import pygame
 from Snake_Module import *
 from Food_Module import *
-import time
 
-
-class Game_Window:
+class GameWindow:
     def __init__(self):
         pygame.init()
-        self.Window = pygame.display.set_mode((800, 600))
+        self.window = pygame.display.set_mode((800, 600))
         pygame.display.set_caption("AI Project")
-        self.snake = Snake(self.Window, 2)
+        self.snake = Snake(self.window, 2)
         self.snake.Draw_Snake()
-        self.food = Food(self.Window)
+        self.food = Food(self.window)
         self.food.Draw_Food()
         self.clock = pygame.time.Clock()
+        self.old_tick = 0
+
+    def time_update(self):
+        ticks = pygame.time.get_ticks()
+        if ticks - self.old_tick > 200:
+            self.snake.Snake_Walk()
+            self.old_tick = ticks
 
     # check if snake ate the food
     def Check_ate_Food(self):
-        if [self.snake.Snake_pos_X[0], self.snake.Snake_pos_Y[0]] == self.food.Food_pos:
-            return True
-        return False
+        return [self.snake.Snake_pos_X[0], self.snake.Snake_pos_Y[0]] == self.food.Food_pos
+         
 
     def Play_Game(self):
         pygame.display.update()
-        self.Window.fill((110, 110, 5))
-        self.clock.tick(10)
-        self.snake.Snake_Walk()
+        self.window.fill((110, 110, 5))
+        self.clock.tick(60)
+        self.time_update()
+
         if self.Check_ate_Food():
             self.snake.increase_Snake_lenght()
             self.food.Move_food()
 
         self.food.Draw_Food()
+        self.snake.Draw_Snake()
 
     # running window win running variable equle to True
     def Run(self, Running=True):
@@ -39,29 +45,28 @@ class Game_Window:
             self.Play_Game()
             # allaw window to receive input from keyboard
             for event in pygame.event.get():
-                if event.type == KEYDOWN:
-                    if event.key == K_ESCAPE:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
                         Running = False
 
-                    if event.key == K_LEFT:
+                    if event.key == pygame.K_LEFT:
                         self.snake.change_direction("left")
 
-                    if event.key == K_RIGHT:
+                    if event.key == pygame.K_RIGHT:
                         self.snake.change_direction("right")
 
-                    if event.key == K_UP:
+                    if event.key == pygame.K_UP:
                         self.snake.change_direction("up")
 
-                    if event.key == K_DOWN:
+                    if event.key == pygame.K_DOWN:
                         self.snake.change_direction("down")
 
                 # when user want to exit window game
-                elif event.type == QUIT:
+                elif event.type == pygame.QUIT:
                     Running = False
 
-            # time.sleep(.3)
 
 
 if __name__ == '__main__':
-    Game = Game_Window()
+    Game = GameWindow()
     Game.Run()
