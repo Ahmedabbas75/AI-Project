556d39105068d90bcc736077893d15741468029c
Ahmedabbas75 <ahmedmassa.757.pyf@gmail.com>
2023-05-04 21:45:49 +0300

    ADD Snake Body and food to wndow game

--- GameWindow.py
+++ GameWindow.py
@@ -1,49 +1,24 @@
 # import module we need in project
-import pygame
 from pygame.locals import *
-
-# class snake style
-class Snake:
-    def __init__(self, Screen):
-        self.parent_screen = Screen
-        self.SnakeHead = pygame.image.load("resources/block.jpg").convert()
-        self.Snake_pos_X = 100 # X coordinate of initial position
-        self.Snake_pos_Y = 100 # Y coordinate of initial position
-        
-    # set new position of the snake head base on left direction
-    def Move_left(self):
-        self.Snake_pos_X -= 10
-        self.Draw()
-
-    # set new position of the snake head base on right direction
-    def Move_right(self):
-        self.Snake_pos_X += 10
-        self.Draw()
-
-    # set new position of the snake head base on up direction
-    def Move_up(self):
-        self.Snake_pos_Y -= 10
-        self.Draw()
-
-    # set new position of the snake head base on down direction
-    def Move_down(self):
-        self.Snake_pos_Y += 10
-        self.Draw()
-
-    # update head of snake
-    def Draw(self):
-        self.parent_screen.fill((110, 110, 5))
-        self.parent_screen.blit(self.SnakeHead,(self.Snake_pos_X,self.Snake_pos_Y))
-        pygame.display.flip() # allow changes to occur
-
+from Snake_Module import *
+from Food_Module import *
+import time
 
 class Game_Window:
     def __init__(self):
         pygame.init()
-        self.Window = pygame.display.set_mode((500, 500))
-        self.snake = Snake(self.Window)
-        self.snake.Draw()
-    
+        self.Window = pygame.display.set_mode((800, 500))
+        pygame.display.set_caption("AI Project")
+        self.snake = Snake(self.Window,10)
+        self.snake.Draw_Snake()
+        self.food = Food(self.Window)
+        self.food.Draw_Food()
+
+
+    def Play_Game(self):
+        self.snake.Snake_Walk()
+        self.food.Draw_Food()
+ 
     # running window win running variable equle to True
     def Run(self, Running = True):
         while Running:
@@ -64,10 +39,14 @@ class Game_Window:
 
                     if event.key == K_DOWN:
                         self.snake.Move_down()
+                 
 
                 # when user want to exit window game
                 elif event.type == QUIT:
                     Running = False
+            
+            self.Play_Game()
+            time.sleep(.2)
 
 if __name__ == '__main__':
     Game = Game_Window()
