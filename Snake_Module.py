fa4b83d425caaf4d2ea2219e0794f9c6a63558ea
Ahmedabbas75 <ahmedmassa.757.pyf@gmail.com>
2023-05-05 06:15:04 +0300

    fix snake colliding itself and frame

--- Snake_Module.py
+++ Snake_Module.py
@@ -7,15 +7,15 @@ class Snake:
         self.Snake_length = Snake_length
         self.parent_screen = Screen
         self.Direction = "right"
-        self.SizeSnakeHead = 40
+        self.sizes_head = 40
 
         # set X,Y coordinates of initial position to snake
-        self.Snake_pos_X, self.Snake_pos_Y = ([self.SizeSnakeHead] * Snake_length), ([self.SizeSnakeHead] * Snake_length)
+        self.snake_pos_x, self.snake_pos_y = ([self.sizes_head] * Snake_length), ([self.sizes_head] * Snake_length)
 
     def increase_Snake_lenght(self):
         self.Snake_length += 1
-        self.Snake_pos_X.append(self.Snake_pos_X[-1])
-        self.Snake_pos_Y.append(self.Snake_pos_Y[-1])
+        self.snake_pos_x.append(self.snake_pos_x[-1])
+        self.snake_pos_y.append(self.snake_pos_y[-1])
 
     # set new position of the snake head base on direction
     def change_direction(self, new_direction):
@@ -24,27 +24,45 @@ class Snake:
     # update head of snake
     def Draw_Snake(self):
         for index in range(self.Snake_length):
-            self.parent_screen.blit(
-                self.SnakeHead, (self.Snake_pos_X[index], self.Snake_pos_Y[index]))
+            self.parent_screen.blit(self.SnakeHead,(self.snake_pos_x[index], self.snake_pos_y[index]))
 
     def Snake_Walk(self):
         # update head of snake
         if self.Direction == "left":
-            self.Snake_pos_X[0] -= self.SizeSnakeHead
+            self.snake_pos_x[0] -= self.sizes_head
 
         if self.Direction == "right":
-            self.Snake_pos_X[0] += self.SizeSnakeHead
+            self.snake_pos_x[0] += self.sizes_head
 
         if self.Direction == "up":
-            self.Snake_pos_Y[0] -= self.SizeSnakeHead
+            self.snake_pos_y[0] -= self.sizes_head
 
         if self.Direction == "down":
-            self.Snake_pos_Y[0] += self.SizeSnakeHead
+            self.snake_pos_y[0] += self.sizes_head
 
         # update body and tail of snake
         for index_body in range(self.Snake_length-1, 0, -1):
-            self.Snake_pos_X[index_body] = self.Snake_pos_X[index_body - 1]
-            self.Snake_pos_Y[index_body] = self.Snake_pos_Y[index_body - 1]
+            self.snake_pos_x[index_body] = self.snake_pos_x[index_body - 1]
+            self.snake_pos_y[index_body] = self.snake_pos_y[index_body - 1]
+
+    # check snake colliding itself or frame
+    def check_colliding(self, x1, y1, x2, y2):
+        if x1 >= x2 and x1 < x2 + self.sizes_head:
+            if y1 >= y2 and y1 < y2 + self.sizes_head:
+                return True
+        return False
+    
+    def is_collision(self):
+        for check_index in range(3,self.Snake_length):
+            if self.check_colliding(self.snake_pos_x[0],self.snake_pos_y[0],
+                        self.snake_pos_x[check_index],self.snake_pos_y[check_index]):
+              raise "Game over"
+
+
+    
+    
+ 
+    
 
  
             
